87ca1eeecaafe09ccfcdaa4aeed6edd05b44b393
CMK Management Group LLC <info@couchcritix.com>
2025-09-09 18:23:53 -0700

    Create page.js

---
+++ app/dashboard/page.js
@@ -0,0 +1,13 @@
+export default function Dashboard() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>Dashboard</h1>
+      <ul>
+        <li>Wage Theft Prevention Notice provided to new hires</li>
+        <li>Harassment Prevention Training tracked</li>
+        <li>Meal/Rest Break policy acknowledged and posted</li>
+        <li>Paystub compliance spot-check complete</li>
+      </ul>
+    </main>
+  );
+}
